af47bc1db61bc4a330d458b9ab33459f76a01912
Casey Sweet <caseysweet83@gmail.com>
2023-08-08 16:09:26 -0400

    Made spots table and migration i also made an association between users and spots just pushing before seeding

---
+++ backend/db/models/spot.js
@@ -0,0 +1,54 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Spot extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+      Spot.belongsTo(models.User, {
+        foreignKey: 'ownerId'
+      })
+    }
+  }
+  Spot.init({
+    address: DataTypes.STRING,
+    city: DataTypes.STRING,
+    state: DataTypes.STRING,
+    country: DataTypes.STRING,
+    lat: DataTypes.INTEGER,
+    lng: DataTypes.INTEGER,
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        goodName(val){
+          if(val === ''){
+            throw new Error("Name can not be blank.")
+          }
+          if(val.startsWith(' ')){
+            throw new Error("Name can not start with a space.")
+          }
+        }
+      }
+    },
+    description: DataTypes.STRING,
+    price: DataTypes.INTEGER,
+    ownerId: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'User',
+        key: 'id',
+      }
+    }
+  }, {
+    sequelize,
+    modelName: 'Spot',
+  });
+  return Spot;
+};
---
+++ backend/db/seeders/20230808190200-spots.js
@@ -0,0 +1,87 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+    */
+    await queryInterface.bulkInsert('Spots', [
+      {
+        name: 'The Sunset',
+        description: 'Nice tiny home on the beach.',
+        price: 124,
+        ownerId: 1,
+        address: '123 Sunset Dr',
+        city: 'South Haven',
+        state: 'Michigan',
+        country: 'United States',
+        lat: 42.401472,
+        lng: -86.288778,
+      },
+      {
+        name: 'The Ultimate Escape',
+        description: 'Right in the center of Mexico City you will have the most fun here.',
+        price: 100,
+        ownerId: 2,
+        address: 'Alto Casa #200 Apt 8',
+        city: 'Mexico City',
+        state: 'Mexico City',
+        country: 'Mexico',
+        lat: 19.431163,
+        lng: -99.137324,
+      },
+      {
+        name: 'Cozy Cool Cabin',
+        description: 'Nice long driveway up to a big snowy cabin.',
+        price: 150,
+        ownerId: 3,
+        address: '231 Cozy Ln',
+        city: 'Packwood',
+        state: 'Washington',
+        country: 'United States',
+        lat: 46.608124,
+        lng: -121.661130,
+      },
+      {
+        name: 'The Perfect Retreat',
+        description: 'Big barndominium sitting on 32 acres!',
+        price: 125,
+        ownerId: 3,
+        address: '132 Barns St',
+        city: 'Allegan',
+        state: 'Michigan',
+        country: 'United States',
+        lat: 42.537225,
+        lng: -85.823725,
+      },
+      {
+        name: 'The Brooklyn Penthouse',
+        description: 'Amazing 4 bedroom penthouse up in the sky with amazing views',
+        price: 250,
+        ownerId: 2,
+        address: '213 Views Dr',
+        city: 'Brooklyn',
+        state: 'New York',
+        country: 'United States',
+        lat: 40.672458,
+        lng: -73.958723,
+      }
+    ], {});
+  },
+
+  async down(queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+  }
+};
