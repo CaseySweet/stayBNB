85fed17222e4a00a9dfe7c64896f70a2688f2b55
Casey Sweet <caseysweet83@gmail.com>
2023-11-14 19:19:05 -0500

    Mostly finished with posting a review

--- frontend/src/components/AReview/index.js
+++ frontend/src/components/AReview/index.js
@@ -3,16 +3,20 @@ import { useDispatch, useSelector } from 'react-redux'
 import * as reviewActions from '../../store/review'
 import { useParams } from 'react-router-dom'
 import OpenModalButton from '../OpenModalButton';
+import PostReview from "../../components/PostReview";
 
 const AReview = () => {
     const dispatch = useDispatch()
     const { spotId } = useParams()
-    const reviews = useSelector(state => state.reviews.review)
-    const [avgStars, setAvgStars] = useState();
-    const sessionUser = useSelector(state => state.session.user);
+    const reviews = useSelector(state => state.reviews.review) || []
+    const [avgStars, setAvgStars] = useState()
+    const sessionUser = useSelector(state => state.session.user)
+    // const spotsObj = useSelector(state => state.spots)
+    // const spots = Object.values(spotsObj)
+
 
     const formatDate = (dateString) => {
-        return new Date(dateString).toLocaleDateString('en-US', { year: 'numeric', month: 'long' });
+        return new Date(dateString).toLocaleDateString('en-US', { year: 'numeric', month: 'long' })
     }
 
     useEffect(() => {
@@ -21,11 +25,22 @@ const AReview = () => {
 
     useEffect(() => {
         if (reviews) {
-            const totalStars = reviews.reduce((sum, review) => sum + review.stars, 0);
-            const average = totalStars / reviews.length;
-            setAvgStars(average);
+            const totalStars = reviews.reduce((sum, review) => sum + review.stars, 0)
+            const average = totalStars / reviews.length
+            setAvgStars(average)
         }
     }, [reviews]);
+
+    const userSignedIn = sessionUser ? sessionUser.id : null
+    // const userHasNoReview = userSignedIn ? reviews.some((review) => console.log(review, "Review")) : false
+    const userHasNoReview = userSignedIn ? reviews.some((review) => review.userId === userSignedIn) : false
+    // const userOwnsSpot = userSignedIn ? spots.some((spot) => console.log(spot, 'userOwnsSpot')) : false
+    // const userOwnsSpot = userSignedIn ? spots.some((spot) => spot.ownerId === userSignedIn) : false
+
+    // console.log("userSignedIn:", userSignedIn);
+    // console.log("userOwnsSpot:", userOwnsSpot);
+
+    const sortedReviews = [...reviews].sort((a, b) => new Date(b.createdAt) - new Date(a.createdAt))
 
     if (!reviews) {
         return (
@@ -34,19 +49,22 @@ const AReview = () => {
     } else {
         return (
             <div>
-                {!reviews || reviews.length === 0 ? (
+                {reviews.length === 0 ? (
                     <div>
                         <strong>New</strong>
                         <div>
-                            {sessionUser && (
+                            {!userHasNoReview && sessionUser && (
                                 <OpenModalButton
-                                buttonText='Post Your Review'
-
-                                    // modalComponent={<PostAReview spotId={spotId} />}
+                                    buttonText='Post Your Review'
+                                    modalComponent={<PostReview spotId={spotId}/>}
                                 />
 
                             )}
-
+                        </div>
+                        <div>
+                            {reviews.length === 0 && sessionUser && (
+                                <div>Be the first to post a review !</div>
+                            )}
                         </div>
                     </div>
 
@@ -57,10 +75,10 @@ const AReview = () => {
                                 ★ {avgStars} • # {reviews.length === 1 ? "1 review" : `${reviews.length} reviews`}
                             </div>
                             <div>
-                                {sessionUser && (
+                                {!userHasNoReview && sessionUser && (
                                     <OpenModalButton
-                                    buttonText='Post Your Review'
-                                        // modalComponent={<PostAReview spotId={spotId} />}
+                                        buttonText='Post Your Review'
+                                        modalComponent={<PostReview spotId={spotId}/>}
                                     />
 
                                 )}
@@ -68,14 +86,15 @@ const AReview = () => {
                         </div>
                     </div>
                 )}
-
-                {reviews.map((review, index) => (
-                    <ul key={index}>
-                        <div>{review.User.firstName}</div>
-                        <div>{formatDate(review.createdAt)}</div>
-                        <div>{review.review}</div>
-                    </ul>
-                ))}
+                <div>
+                    {sortedReviews.map((review, index) => (
+                        <ul key={index}>
+                            <div>{review.User.firstName}</div>
+                            <div>{formatDate(review.createdAt)}</div>
+                            <div>{review.review}</div>
+                        </ul>
+                    ))}
+                </div>
             </div>
         )
     }
